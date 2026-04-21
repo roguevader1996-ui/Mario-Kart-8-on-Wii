d489db8bd0c22b2108e9542adc46504db6fc49f6
roguevader1996-ui <roguevader1996@gmail.com>
2026-04-21 13:31:26 +0200

    Remove unneeded settings

--- PulsarEngine/Settings/SettingsParam.cpp
+++ PulsarEngine/Settings/SettingsParam.cpp
@@ -8,16 +8,16 @@ namespace Pulsar {
 namespace Settings {
 
 u8 Params::radioCount[Params::pageCount] ={
-    3, 5, 3, 5, 2 //menu, race, host, OTT, KO
+    3, 4, 3, 5, 2 //menu, race, host, OTT, KO
     //Add user radio count here
 
 };
-u8 Params::scrollerCount[Params::pageCount] ={ 1, 1, 1, 0, 2 }; //menu, race, host, OTT, KO
+u8 Params::scrollerCount[Params::pageCount] ={ 1, 0, 1, 0, 2 }; //menu, race, host, OTT, KO
 
 u8 Params::buttonsPerPagePerRow[Params::pageCount][Params::maxRadioCount] = //first row is PulsarSettingsType, 2nd is rowIdx of radio
 {
     { 2, 2, 3, 0, 0, 0 }, //Menu 
-    { 2, 2, 2, 2, 3, 0 }, //Race
+    { 2, 2, 2, 2, 0, 0 }, //Race
     { 2, 4, 2, 0, 0, 0 }, //Host
     { 3, 3, 2, 2, 2, 0 }, //OTT
     { 2, 2, 0, 0, 0, 0 }, //KO
@@ -27,7 +27,7 @@ u8 Params::buttonsPerPagePerRow[Params::pageCount][Params::maxRadioCount] = //fi
 u8 Params::optionsPerPagePerScroller[Params::pageCount][Params::maxScrollerCount] =
 {
     { 5, 7, 0, 0, 0}, //Menu 
-    { 4, 0, 0, 0, 0}, //Race
+    { 0, 0, 0, 0, 0}, //Race
     { 7, 0, 0, 0, 0}, //Host
     { 0, 0, 0, 0, 0}, //OTT
     { 4, 4, 0, 0, 0}, //KO
@@ -36,6 +36,3 @@ u8 Params::optionsPerPagePerScroller[Params::pageCount][Params::maxScrollerCount
 
 }//namespace Settings
 }//namespace Pulsar
-
-
-
